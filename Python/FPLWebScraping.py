9a20ec6f19fc499b9f71d66c09488a760ade010e
midhun thaduru <tmidhunr@gmail.com>
2016-11-01 19:47:48 +0530

    Added code to scrape through URL for a single file

--- Python/FPLWebScraping.py
+++ Python/FPLWebScraping.py
@@ -1,14 +1,18 @@
+import csv
+import time
 import urllib2
 import cookielib
+import dryscrape
 from bs4 import BeautifulSoup
-import csv
 
-STATS_TYPE_url = ["event_points", "minutes", "goals_scored", "assists", "clean_sheets", "goals_conceded", 
-				  "own_goals","penalties_saved", "penalties_missed", "yellow_cards", "red_cards", 
-				  "saves", "bonus", "bps", "influence", "creativity", "threat", "ict_index", 
-				  "dreamteam_count", "value_form", "value_season", "points_per_game", "transfers_in", 
-				  "transfers_out", "transfers_in_event", "transfers_out_event", "cost_change_start", 
-				  "cost_change_start_fall", "cost_change_event", "cost_change_event_fall"]
+# STATS_TYPE_url = ["event_points", "minutes", "goals_scored", "assists", "clean_sheets", "goals_conceded", 
+# 				  "own_goals","penalties_saved", "penalties_missed", "yellow_cards", "red_cards", 
+# 				  "saves", "bonus", "bps", "influence", "creativity", "threat", "ict_index", 
+# 				  "dreamteam_count", "value_form", "value_season", "points_per_game", "transfers_in", 
+# 				  "transfers_out", "transfers_in_event", "transfers_out_event", "cost_change_start", 
+# 				  "cost_change_start_fall", "cost_change_event", "cost_change_event_fall"]
+
+STATS_TYPE_url = ["minutes"]
 
 # TEAMS_DICT = {"Arsenal":"te_1", "Bournemouth":"te_2","Burnley":"te_3","Chelsea":"te_4","Crystal Palace":"te_5",
 # "Everton":"te_6", "Hull":"te_7", "Leicester":"te_8", "Liverpool":"te_9", "Man City":"te_10", "Man Utd":"te_11",
@@ -28,27 +32,36 @@ Table_columns = ['player_name', 'player_team', 'player_location', 'now_cost', 's
 
 for stats_type in STATS_TYPE_url:
 	for key in sorted(TEAMS_DICT):
-		file = csv.writer(open(key+"/"+stats_type+".csv", "w"))
-		url = "/home/midhunthaduru/Dropbox/Kaggle/FootBallAnalytics/"+key+"/"+stats_type+".html"
-		# url = "https://fantasy.premierleague.com/a/statistics/"+stats_type+"/"+TEAMS_DICT[key]
+		file = csv.writer(open("Teams/"+key+"/"+stats_type+".csv", "w"))
+		# url = "/home/midhunthaduru/Dropbox/Kaggle/FootBallAnalytics/"+key+"/"+stats_type+".html"
+		url = "https://fantasy.premierleague.com/a/statistics/"+stats_type+"/"+TEAMS_DICT[key]
+
+		# session.set_timeout(30)
+		session = dryscrape.Session()
+
+		session.visit(url)
+		response = session.body()
+		
 
 		# writing column names of csv file
 		Table_columns.append(stats_type)
 		file.writerow(Table_columns)
 		Table_columns.pop()
 
-		# requesting from the web to avoid 403 error
+		# # requesting from the web to avoid 403 error
 		# req = urllib2.Request(url, headers=hdr)
+
 		# try:
 		#     page = urllib2.urlopen(req)
 		# except urllib2.HTTPError, e:
 		#     print e.fp.read()
 
-		page = open(url, "r").read()
-		# # page = urllib2.urlopen(url, 'r')
+		# page = open(url, "r").read()
+		# page = urllib2.urlopen(url, 'r')
+		# soup = BeautifulSoup(page, "lxml")
 
-		soup = BeautifulSoup(page, "lxml")
-		# # print soup.prettify()
+		soup = BeautifulSoup(response, "lxml")
+		# print soup.prettify()
 
 		MainContent = soup.findAll('div', {'class':'table ism-scroll-table'})
 		# print MainContent
@@ -89,6 +102,11 @@ for stats_type in STATS_TYPE_url:
 						print "bad string"
 						continue
 
+					# print player_name, player_team, player_location, val, sel_pct, form, pts, col
+
 					# Writing to extracted values to csv file
 					file.writerow([player_name, player_team, player_location, 
 						val, sel_pct, form, pts, col])
+
+		session.reset()
+		# time.sleep(30)
